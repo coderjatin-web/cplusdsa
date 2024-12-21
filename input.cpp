d76ebb05f6cdcf444888ec57ea39de3b99fc04a5
Jatin Chourasiya <jatinchourasiya01@gmail.com>
2024-12-21 13:47:00 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+       int x; // x nam ka ek dabba banaya 
+       cout<<"enter a number :";
+       cin>>x; //input lia x nam ke dabbe me
+       cout<<"the value of x is :"<<x;
+} 
+ 
