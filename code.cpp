3bccd9d7b03efba9a464e9534b2596b49a052097
Jatin Chourasiya <jatinchourasiya01@gmail.com>
2024-12-21 13:48:23 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,50 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+    cout<<"hello world";
+    cout<<endl;   // same as \n remeber ki "" nhi use hota isme 
+    cout<<"hello world";
+    return 0;   //optional
+}*/
+
+      //c++ me use <<endl
+
+
+/*#include<iostream>
+using namespace std;
+int main() {
+    cout<<"hello world\n\n\n";
+       
+    cout<<"hello world";
+    return 0;   //optional
+}*/
+
+
+
+
+/*#include<iostream>
+using namespace std;
+int main() {
+    cout<<"hello world"<<endl;   //escape sequence
+    cout<<"hello world";
+    return 0;   //optional
+}*/
+
+
+
+/*#include<iostream>
+using namespace std;
+int main() {
+    cout<<"hello world"<<endl<<endl<<"hello world";   //escape sequence
+    return 0;   //optional
+}*/
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"5+8"<<endl;   //only print //
+    cout<<5+8;           //actual calculations//
+    return 0;   //optional
+}
+
