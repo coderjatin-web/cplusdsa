eec046ddcbdb8fa0df510bff778a45ef50ef20ce
Jatin Chourasiya <jatinchourasiya01@gmail.com>
2024-12-21 13:48:02 +0530

    Add files via upload

---
+++ hiearchy.cpp
@@ -0,0 +1,9 @@
+// hiearchy me simply order ha ---> 1(). 2 *,/,% 3.+,- 4.<,>,<=,>= 5. ==,!= 
+ // and agar symbol same to left to right move karo
+
+#include<iostream>
+using namespace std;
+int main (
+){
+    
+}
