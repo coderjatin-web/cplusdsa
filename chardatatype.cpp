8e9caba64b151838419243ac87714f9940f0e255
Jatin Chourasiya <jatinchourasiya01@gmail.com>
2024-12-21 13:47:36 +0530

    Add files via upload

---
+++ chardatatype.cpp
@@ -0,0 +1,108 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    char ch = '\'; ye isme error dikha rha ha 
+    cout<<ch;
+
+}*/
+
+
+
+ /*#include<iostream>
+  using namespace std;
+ int main()
+ {
+     int x,y;
+     x=10;
+     y=11;
+     cout<<x<<" "<<y;// ye hamne gap dene ke liye kia ya fir ham end l ka bhi use kr skte hein  ****nhi bhai galat smjh liye apn endl line chorne ke liye hotta ha or isme apan ne space chora ha 
+
+
+ }*/
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char ch = 'a'; 
+//     char x = 'b';
+//     cout<<ch<<endl<<x;
+
+
+// }
+
+ /*#include<iostream>
+  using namespace std;
+ int main()
+  {
+      char ch = 'a';   
+     cout<<"Enter  alphabet :"   ;   // ye batane ke liye ki konse no. ka alphabet ha 
+      cin>>ch;
+      cout<<(int)ch-64 ;
+  }*/
+
+
+ /* #include<iostream>
+  using namespace std;
+ int main()
+  {
+      char ch;   
+      cout<<"Enter  alphabet :"   ;   //same as abobe bas tycasting ki help se kr rha hu 
+      cin>>ch;
+      int x=(int)ch;  //typecasting ---> char ko int me convert
+      cout<<x-64;
+  }*/
+
+
+//      ///    TYPECASTING QUESTIONS      ////
+// // TAKE INTGER AS INPUT AND PRINT HALF OF IT 
+// // #include<iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int x;
+// //     cout<<"Enter value of integer :";
+// //     cin>>x;
+// //     float y= (float)x;
+// //     cout<<y/2;      //// maine isme cout<<x/2 krdia to GIF hi aya
+// //                     //// hum chahe to direct float y= x kr skte hein but c++ me typcasting krni hoti ha wo hamara fundamental ha , but hamara vs code advance ha jiske krn bina typcasting ke bhi ho ja rha ha 
+// // }
+
+
+
+// question no. 2
+//atke float as input and print fractional part of it 
+
+ /*#include<iostream>
+ using namespace std;
+ int main()
+ {
+     float x;
+    cout<<"Enter the value :";
+    cin>>x;
+       int y= (int)x;
+    float z= x-y;
+    if (x<0)
+
+    {
+        z=(-1*y+1)+x;
+        
+    }
+    
+    
+    
+     cout<<"its fractional part is : "<<z;
+}*/
+
+
+
+/*#include<iostream>
+ using namespace std;
+ int main()
+ {
+     int x;
+     cin>>x;
+     cout<<x;     //mtlb int no. ka gif nhi deta wrna iska ans to fir -2 ana chahiye tha 
+     // mtlb simply int na  integral part bas print krke deta ha
+ }*/
